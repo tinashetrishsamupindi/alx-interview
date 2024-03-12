61be8db1686d694e1f03d7a590c89c282a6adae0
tinashetrishsamupindi <tinashetrishsamupindi@gmail.com>
2024-03-12 00:04:07 -0700

    0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Prime Game: Determine the winner of the Prime Game.
+"""
+
+
+def is_prime(num):
+    """Check if a number is prime."""
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def isWinner(x, nums):
+    """Determine the winner of the Prime Game."""
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        prime_count = sum(1 for i in range(1, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
+
+
+# Example usage
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
